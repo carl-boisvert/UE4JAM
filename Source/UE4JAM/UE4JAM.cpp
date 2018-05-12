00c78344160e1404e2be8d583b73872ceb1a6b8a
Snappydue <cboisvert@egn.tv>
2018-05-11 22:05:40 -0400

    Projet de base

---
+++ Source/UE4JAM/UE4JAM.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4JAM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4JAM, "UE4JAM" );
---
+++ Source/UE4JAM/UE4JAMGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4JAMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4JAM_API AUE4JAMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
